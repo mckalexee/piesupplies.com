2415d63bd7a661119bf1d8c3bee2c36b3455496f
Alex McKenzie <mcalex@hey.com>
2021-07-23 17:33:49 -0400

    skip waiting on the service worker

--- workbox-config.js
+++ workbox-config.js
@@ -1,11 +1,7 @@
 module.exports = {
-	globDirectory: 'site/',
-	globPatterns: [
-		'**/*.{png,xml,ico,jpg,html,css,svg,webmanifest}'
-	],
-	ignoreURLParametersMatching: [
-		/^utm_/,
-		/^fbclid$/
-	],
-	swDest: 'site/sw.js'
+  globDirectory: "site/",
+  globPatterns: ["**/*.{png,xml,ico,jpg,html,css,svg,webmanifest}"],
+  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
+  swDest: "site/sw.js",
+  skipWaiting: true,
 };
